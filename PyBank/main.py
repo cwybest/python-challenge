c4cdd52d9b5880e0ab1c3320b6cf142faf856849
cwybest <wanyuchang1207@gmail.com>
2020-09-09 20:26:21 -0400

    Work in Progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,24 @@ import os
 import csv
 csvpath = os.path.join('Resources','budget_data.csv')
 with open(csvpath)as csvfile:
-    csvreader = csv.reader(csvfile,delimiter =",")
+    csvreader = csv.reader(csvfile, delimiter =",")
     header = next(csvreader)
-    revenue = [float(row[1]) for row in csvreader]
-    months = [str(row[0]) for row in csvreader]
+    revenue = []
+    months = []
+    for row in csvreader:
+        revenue.append(float(row[1]))
+        months.append(str(row[0]))
     counter = len(revenue)
     net_total = sum(revenue)
     avg_changes = (revenue[-1]-revenue[0])/counter
     monthly_change = [revenue[i]-revenue[i-1]for i in range(counter)]
-    #greatest_increase =months[monthly_change.index(max(monthly_change))]
-    for row in csvreader:
-        print(row)
-   
+    value_of_increase = max(monthly_change)
+    month_of_increase =months[monthly_change.index(value_of_increase)]
+    value_of_decrease = min(monthly_change)
+    month_of_decrease =months[monthly_change.index(value_of_decrease)]
+with open(os.path.join('Analysis',"analysis"), "w+") as f:
+    def results():
+        text = f'Financial Analysis\nTotal Months: {counter}\nTotal: {net_total}\nAverage Change: {avg_changes}\nGreatest Increase in Profit: {month_of_increase}, {value_of_increase}\nGreatest Decrease in Profits:{month_of_decrease}, {value_of_decrease}'
+        return text
+    f.write(results())
+print(results())
